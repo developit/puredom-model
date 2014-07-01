5ed8bb44ffd8b28ee5336fec4d95d925b08c838d
Jason Miller <jason@developit.ca>
2014-07-01 16:28:45 -0400

    0.3.0: Add test runner and tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,11 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+
 		uglify: {
 			main : {
 				options: {
-					banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+					banner: '/*! <%= pkg.name %> <%= pkg.version %> */\n'
 				},
 				files: {
 					'puredom.Model.min.js': [
@@ -14,22 +15,81 @@ module.exports = function(grunt) {
 				}
 			}
 		},
+
 		jshint : {
 			options : {
-				browser : true
+				'browser' : true
 			},
-			main : [
-				'puredom.Model.js'
-			]
+			main : {
+				options : {
+					'-W041' : true,
+					'-W030' : true
+				},
+				src : ['puredom.Model.js']
+			},
+			test : {
+				options : {
+					'-W030' : true
+				},
+				src : [
+					'test/**/*.js',
+					'!test/js/**/*'
+				]
+			}
+		},
+
+		connect : {
+			test : {
+				options : {
+					port : 9092,
+					base : '.'
+				}
+			}
+		},
+
+		mocha : {
+			test : {
+				options : {
+					run : true,
+					reporter : 'Spec',
+					urls : [
+						'http://localhost:9092/test/index.html'
+					]
+				}
+			}
+		},
+
+		watch : {
+			options : {
+				interrupt : true
+			},
+			src : {
+				files : ['puredom.Model.js'],
+				tasks : ['default']
+			},
+			test : {
+				files : ['test/**/*'],
+				tasks : ['test']
+			}
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
+	require('load-grunt-tasks')(grunt);
 
 	grunt.registerTask('default', [
+		'build',
+		'test'
+	]);
+
+	grunt.registerTask('build', [
 		'jshint:main',
 		'uglify:main'
+	]);
+
+	grunt.registerTask('test', [
+		'jshint:test',
+		'connect:test',
+		'mocha:test'
 	]);
 	
 };
